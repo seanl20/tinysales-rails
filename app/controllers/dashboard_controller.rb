ce0108d89363467657b4949b796e8334ea65ef6b
sean <seanl20@yahoo.com>
2025-04-15 00:19:19 +1000

    feat: add el transition importmap

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,9 +2,5 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    flash.now[:notice] = {
-      title: "TEST",
-      body: "TEST"
-    }
   end
 end
