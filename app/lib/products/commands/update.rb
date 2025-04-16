27e9994f039273f2d17b121f7aca4345c1d39f3f
sean <seanl20@yahoo.com>
2025-04-16 19:34:24 +1000

    feat: add price and url to be updated for products

---
+++ app/lib/products/commands/update.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Products
+  module Commands
+    class Update < Command
+      def call(id:, params:)
+        attrs = Products::Changesets::Update.map(params).merge({
+          price: params[:price]
+        })
+
+        yield update_products(id:, attrs:)
+
+        Success(product: Repositories::ProductRepo.new.get(id: params[:slug]))
+      end
+
+      def update_products(id:, attrs:)
+        Success(Repositories::ProductRepo.new.update(id:, attrs:))
+      rescue ActiveRecord::RecordInvalid
+        Failure(:invalid)
+      end
+    end
+  end
+end
