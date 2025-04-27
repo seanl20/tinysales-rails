864c0800b0318224cbd81d21ccbac0a135e9c770
sean <seanl20@yahoo.com>
2025-04-27 11:53:33 +1000

    feat: implement file upload

--- app/controllers/api/contents_controller.rb
+++ app/controllers/api/contents_controller.rb
@@ -1,11 +1,15 @@
 module Api
   class ContentsController < ApplicationController
-    before_action :authenticate_user!
+    protect_from_forgery with: :null_session
+    # before_action :authenticate_user!
 
     def create
-      @content = Contents::Commands::Create.new.call(params: content_params)
+      result = Contents::Commands::Create.new.call(params: content_params)
 
-      render json: ContentSerializer.new(@content).serializable_hash.to_json
+      case result
+      in Success(content:)
+        render json: ContentSerializer.new(content).serializable_hash.to_json
+      end
     end
 
     private
