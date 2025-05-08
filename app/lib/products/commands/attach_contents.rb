ca1d1daa6915a86de79dbc8e01cb23e8e7fe26b2
sean <seanl20@yahoo.com>
2025-05-08 22:54:10 +1000

    feat: attach contents to product

--- app/lib/products/commands/attach_contents.rb
+++ app/lib/products/commands/attach_contents.rb
@@ -4,15 +4,17 @@ module Products
   module Commands
     class AttachContents < Command
       def call(content_ids:, product_id:, content_params:)
-        content_params.map do |content_param|
+        content_params[:contents].each do |content_param|
           update_content(content_param:, product_id:)
         end
+
+        Repositories::ProductRepo.new.get(id: product_id)
       end
 
       def update_content(content_param:, product_id:)
         attrs = {
           name: content_param[:name],
-          description: content_param[:description],
+          description: content_param[:description].blank? ? nil : content_param[:description],
           product_id:
         }
 
